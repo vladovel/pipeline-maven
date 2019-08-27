16c21d5e85722b104c3a8339cb29a5a0e8f5666f
Jenkins <jenkins@localhost.localdomain>
2019-08-27 01:33:06 -0400

    Removed option for interactive TTY

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -6,4 +6,4 @@ echo "***   Building JAR   *************"
 WORKSPACE=/home/jenkins/jenkins_data/jenkins_home/workspace/pipeline-docker-maven
 
 echo "***   Building JAR   *************"
-docker run --rm -ti -v $WORKSPACE/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3-alpine "$@" 
+docker run --rm -i -v $WORKSPACE/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3-alpine "$@" 
--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -5,4 +5,4 @@ echo "***   Testing JAR   *************"
 
 WORKSPACE=/home/jenkins/jenkins_data/jenkins_home/workspace/pipeline-docker-maven
 
-docker run --rm -ti -v $PWD/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3-alpine "$@" 
+docker run --rm -i -v $PWD/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3-alpine "$@" 
