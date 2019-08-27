e86d89e916cf94cf137513136adac87d000572f1
Jenkins <jenkins@localhost.localdomain>
2019-08-27 01:22:35 -0400

    Fixed path for workspace execution

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -2,4 +2,8 @@
 
 echo "**********************************"
 echo "***   Building JAR   *************"
-docker run --rm -ti -v $PWD/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3-alpine "$@" 
+
+WORKSPACE=/home/jenkins/jenkins_data/jenkins_home/workspace/pipeline-docker-maven
+
+echo "***   Building JAR   *************"
+docker run --rm -ti -v $WORKSPACE/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3-alpine "$@" 
--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -2,4 +2,7 @@
 
 echo "**********************************"
 echo "***   Testing JAR   *************"
+
+WORKSPACE=/home/jenkins/jenkins_data/jenkins_home/workspace/pipeline-docker-maven
+
 docker run --rm -ti -v $PWD/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3-alpine "$@" 
